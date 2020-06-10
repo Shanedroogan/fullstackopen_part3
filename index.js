8f74df1cdf64e1add2a0fea4dc8a4a13f35cac55
shanedroogan <shanedroogan@gmail.com>
2020-06-10 12:47:34 -0400

    fixed express.static in index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors')
 
 app.use(express.json())
+app.use(express.static('build'))
 app.use(cors())
 
 morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
